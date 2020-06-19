3757a099e4b6ebe0aaddc5e7d5b74198f76e7bee
Brock Chelle <Brocklchelle@gmail.com>
2020-06-18 23:12:26 -0600

    Sunset animation added changed header styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import Header from './components/settings/header.component';
+import Welcome from './components/welcome/welcome.component';
+import Input from './components/input/input.component';
 import City from './components/city/city.component'
 import './App.css';
 
 function App() {
 
   const [citySize, setCitySize] = useState(10);
-  const [shuffleCity, setShuffleCity ] = useState(false)
+  const [shuffleCity, setShuffleCity ] = useState(false);
+  const [firstTime, setFirstTime] = useState(window.localStorage.getItem('firstTime'));
 
   const handleCitySizeChange = (event, newValue) => {
     setCitySize(newValue);
@@ -15,7 +17,14 @@ function App() {
   return (
     <div className="App">
       <div className="sun" />
-      <Header sliderValue={citySize} changeHandler={handleCitySizeChange} clickHandler={() => setShuffleCity(!shuffleCity)}></Header>
+      {
+        firstTime === null ? 
+        <Welcome clickHandler={() => {
+          window.localStorage.setItem('firstTime', 'false')
+          setFirstTime(false)
+        }}/> : 
+        <Input sliderValue={citySize} changeHandler={handleCitySizeChange} clickHandler={() => setShuffleCity(!shuffleCity)}/>
+      }
       <City numBuildings={citySize} shuffleCity={shuffleCity}/>
     </div>
   );
---
+++ src/components/welcome/welcome.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles'
+import { Check } from '@material-ui/icons/';
+import { orange } from '@material-ui/core/colors';
+import './welcome.styles.css'
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(orange[500]),
+    backgroundColor: orange[500],
+    '&:hover': {
+      backgroundColor: orange[700],
+    },
+  },
+}))(Button);
+
+export default function Welcome({clickHandler}) {
+  return (
+    <div className="welcome">
+      <h1 className="title">Sunset Boulevard</h1>
+      <p className="description">
+        Welcome to sunset boulevard! Where the only way you can see the sunset is by having no taller buildings to the west. 
+        Luckily, Coder Foundry has an agreement with the city that always gives them the top floor of the tallest building
+        so that Bobby and Kevin can watch the sun set every night.
+      </p>
+      <ColorButton variant="contained" color="primary" endIcon={<Check />} onClick={clickHandler}>
+        Continue
+      </ColorButton>
+    </div>
+  )
+}
